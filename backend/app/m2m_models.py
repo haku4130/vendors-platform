0519bda68d34f69c6dac03eb8f6b0e72d2a85c66
Andrey Osipov <a.osipov.code@gmail.com>
2025-11-13 18:13:59 +0300

    Add models for Vendor, Project, Service and service Category

---
+++ backend/app/m2m_models.py
@@ -0,0 +1,21 @@
+import uuid
+
+from sqlmodel import Field, SQLModel
+
+
+class ProjectServiceLink(SQLModel, table=True):
+    project_id: uuid.UUID | None = Field(
+        default=None, foreign_key="project.id", primary_key=True
+    )
+    service_id: uuid.UUID | None = Field(
+        default=None, foreign_key="service.id", primary_key=True
+    )
+
+
+class VendorServiceLink(SQLModel, table=True):
+    vendor_profile_id: uuid.UUID | None = Field(
+        default=None, foreign_key="vendorprofile.id", primary_key=True
+    )
+    service_id: uuid.UUID | None = Field(
+        default=None, foreign_key="service.id", primary_key=True
+    )
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,12 +1,25 @@
+import enum
 import uuid
+from datetime import date
+from typing import Optional
 
 from pydantic import EmailStr
 from sqlmodel import Field, Relationship, SQLModel
 
+from .m2m_models import ProjectServiceLink, VendorServiceLink
 
-# Shared properties
+
+class UserRole(str, enum.Enum):
+    vendor = "vendor"
+    company = "company"
+
+
 class UserBase(SQLModel):
     email: EmailStr = Field(unique=True, index=True, max_length=255)
+    company_name: str | None = Field(default=None, max_length=255)
+    location: str | None = Field(default=None, max_length=255)
+    logo_url: str | None = Field(default=None, max_length=255)
+    role: UserRole | None = None
     is_active: bool = True
     is_superuser: bool = False
     full_name: str | None = Field(default=None, max_length=255)
@@ -32,6 +45,9 @@ class UserUpdate(UserBase):
 class UserUpdateMe(SQLModel):
     full_name: str | None = Field(default=None, max_length=255)
     email: EmailStr | None = Field(default=None, max_length=255)
+    location: str | None = Field(default=None, max_length=255)
+    logo_url: str | None = Field(default=None, max_length=255)
+    company_name: str | None = Field(default=None, max_length=255)
 
 
 class UpdatePassword(SQLModel):
@@ -44,6 +60,10 @@ class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
     items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
+    vendor_profile: Optional["VendorProfile"] = Relationship(
+        back_populates="user"
+    )  # Вынужденно используем Optional, так как использование "VendorProfile | None" невозможно
+    projects: list["Project"] = Relationship(back_populates="owner")
 
 
 # Properties to return via API, id is always required
@@ -54,6 +74,29 @@ class UserPublic(UserBase):
 class UsersPublic(SQLModel):
     data: list[UserPublic]
     count: int
+
+
+class VendorProfileBase(SQLModel):
+    employee_count: int | None = None
+    founded_year: int | None = None
+    turnover: float | None = None
+    description: str | None = Field(default=None, max_length=2000)
+
+
+class VendorProfile(VendorProfileBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    user_id: uuid.UUID = Field(
+        foreign_key="user.id", unique=True, ondelete="SET NULL", nullable=True
+    )
+    user: User | None = Relationship(back_populates="vendor_profile")
+    services: list["Service"] = Relationship(
+        back_populates="vendors", link_model=VendorServiceLink
+    )
+
+
+class VendorProfilePublic(VendorProfileBase):
+    id: uuid.UUID
+    user_id: uuid.UUID
 
 
 # Shared properties
@@ -90,6 +133,65 @@ class ItemPublic(ItemBase):
 class ItemsPublic(SQLModel):
     data: list[ItemPublic]
     count: int
+
+
+class CategoryBase(SQLModel):
+    label: str = Field(max_length=255)
+
+
+class Category(CategoryBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    services: list["Service"] = Relationship(back_populates="category")
+
+
+class ServiceBase(SQLModel):
+    label: str = Field(max_length=255)
+
+
+class Service(ServiceBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    category_id: uuid.UUID = Field(
+        foreign_key="category.id", ondelete="SET NULL", nullable=True
+    )
+    category: Category | None = Relationship(back_populates="services")
+
+    projects: list["Project"] = Relationship(
+        back_populates="services", link_model=ProjectServiceLink
+    )
+    vendors: list[VendorProfile] = Relationship(
+        back_populates="services", link_model=VendorServiceLink
+    )
+
+
+class ProjectBase(SQLModel):
+    title: str = Field(min_length=1, max_length=255)
+    description: str | None = Field(default=None, max_length=2000)
+    start_date: date | None = None
+    location: str | None = Field(default=None, max_length=255)
+    website: str | None = Field(default=None, max_length=255)
+
+
+class Project(ProjectBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    owner_id: uuid.UUID = Field(
+        foreign_key="user.id", ondelete="SET NULL", nullable=True
+    )
+    owner: User | None = Relationship(back_populates="projects")
+    services: list[Service] = Relationship(
+        back_populates="projects", link_model=ProjectServiceLink
+    )
+
+
+class ProjectPublic(ProjectBase):
+    id: uuid.UUID
+    owner_id: uuid.UUID
+    services: list[Service] | None = None
+
+
+class ProjectsPublic(SQLModel):
+    data: list[ProjectPublic]
+    count: int
+    services: list[Service] | None = None
 
 
 # Generic message
